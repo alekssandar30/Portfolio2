a3f939bb206e97659f183c1f2d128c979afb0045
Aleksandar <sale.novakovic403@gmail.com>
2019-09-19 12:10:46 +0200

    first page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 //start
+
+const selectElement = (s) => document.querySelector(s);
+
+//open menu
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+//close menu
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
